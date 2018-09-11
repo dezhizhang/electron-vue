e7f3de83f720db46aad7b0b4457a3277c3aa39e1
dezhizhang <1541609448@qq.com>
2018-09-11 21:52:10 +0800

    修改

--- src/renderer/components/header/Header.vue
+++ src/renderer/components/header/Header.vue
@@ -1,30 +1,12 @@
 <template>
-  <div>
+ 
     <div class='header'>
       <div class='header-wapper'>
           <i @click='handleMain' class='iconfont icon-zuixiaohua'></i>
           <i @click='handleMax' class='iconfont icon-zuidahua'></i>
           <i @click='handleClose' class='iconfont icon-close'></i>
       </div>
-      
     </div>
-    <div class='header-content'>
-        <input type='text' v-model='name' placeholder='请输入用户名'/>
-        <input type='text' v-model='age' placeholder='请输入年龄'/>
-    </div>
-    <div>
-      <el-button type='primary' @click='addData()'>增加数据</el-button>
-      <el-button type='success' @click='editData()'>修改数据</el-button>
-      <el-button type='info' @click='searchData()'>查找数据</el-button>
-      <el-button type='danger' @click='deleteData()'>删除数据</el-button>
-    </div>
-    <div>
-      <ul v-for='(item,index) in list'>
-        <li>{{item.name}}</li>
-        <li>{{item.age}}</li>
-      </ul>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -49,46 +31,7 @@ export default{
         this.$electron.ipcRenderer.send('window-close')
 
       },
-      addData () {
-        this.$db.insert({'name':this.name,'age':this.age},(err,data) => {
-            if(err){
-              console.log(err);
-              return;
-
-            }
-            console.log(data);
-
-        })
-
-      },
-      editData(){
-        
-      },
-      searchData(){
-          this.$db.find({ },(err,data) => {
-            if(err){
-              console.log(err);
-              return;
-
-            }
-            //console.log(data);
-            this.list=data;
-
-
-          })
-      },
-      deleteData(){
-          this.$db.remove({name:'11111'},(err,data) => {
-            if(err){
-              console.log(err);
-              return;
-
-            }   
-            console.log(data);
-
-          })
-      }
-      
+     
     }
 }
 </script>
