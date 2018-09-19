c779c33ef148e23bac9297313a26b43c0ef41cb8
dezhizhang <1541609448@qq.com>
2018-09-19 23:04:01 +0800

    增加时获取签名

--- src/renderer/components/keywords/Keywords.vue
+++ src/renderer/components/keywords/Keywords.vue
@@ -3,42 +3,38 @@
      <div class='keywords-btn'>
         <el-button size='small' type='primary' @click='addKeyWords'>增加关建字</el-button>
      </div>
-     <div class='keywords-form'>
-        <el-dialog title="收货地址" :visible.sync="keywordsVisible">
+     <div class='keywords-form'> 
+        <el-dialog title="增加关建词" center :visible.sync="keywordsVisible">
             <el-form :model="form">
-              <el-form-item label="标题" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
-              </el-form-item>
-              <el-form-item label="必须包含关建字" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
+              <el-form-item label="必须包含关建字" :label-width="formLabelWidth">  
+                <el-input v-model="form.keyword" autocomplete="off"></el-input>
               </el-form-item>
               <el-form-item label="可包含关建字" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
+                <el-input v-model="form.may_keywor" autocomplete="off"></el-input>
               </el-form-item>
               <el-form-item label="不包含关建字" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
+                <el-input v-model="form.nokeyword" autocomplete="off"></el-input>
               </el-form-item>
-              <el-form-item label="监控效果" :label-width="formLabelWidth">
-                <el-input v-model="form.name" autocomplete="off"></el-input>
+              <el-form-item label="监控频率" :label-width="formLabelWidth">
+                <el-input v-model="form.frequency" autocomplete="off"></el-input>
               </el-form-item>
              </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button size='small' @click="dialogFormVisible = false">取 消</el-button>
-                <el-button size='small' type="primary" @click="keywordsVisible = false">确 定</el-button>
+                <el-button size='small' type="primary" @click="addKeyWordsSubmit">确 定</el-button>
             </div>
         </el-dialog>
      </div>
      <div class='keywords-table'>
         <el-table :data="tableData" border style="width: 100%">
-          <el-table-column prop="date" label="标题" width="190">
+         
+          <el-table-column prop="keyword" label="必须包含关建字" width="240">
           </el-table-column>
-          <el-table-column prop="name" label="必须包含关建字" width="190">
+          <el-table-column prop="may_keywor" label="可包含关建字" width="240">
           </el-table-column>
-          <el-table-column prop="address" label="可包含关建字" width="190">
+           <el-table-column prop="nokeyword" label="不包含关建字" width="240"> 
           </el-table-column>
-           <el-table-column prop="address" label="不包含关建字" width="190"> 
-          </el-table-column>
-           <el-table-column prop="address" label="监控效果" width="190">
+           <el-table-column prop="frequency" label="监控频率" width="240">
           </el-table-column>
         </el-table> 
      </div>
@@ -50,13 +46,39 @@
 </template> 
 
 <script>
+import tools from '../../model/tools.js'
 export default {
   name:'Keywords',
   methods:{
-      addKeyWords(){
+      addKeyWords() {
         this.keywordsVisible = true;
         
 
+      },
+      addKeyWordsSubmit() {
+        //http://www.apiying.com/yuqing/index.php?m=Api&a=addKeywords
+        let userInfo = tools.stroage.get('userInfo')
+       
+        let sign = tools.sign({
+          'a':'addKeywords',x
+          'uid':userInfo.id,
+          'salt':userInfo.salt,
+        });
+
+        this.$http.post(tools.config.apiUrl+'/index.php?m=Api&a=addKeywords', {
+            keyword: this.form.keyword,
+            may_keyword: this.form.may_keyword,
+            nokeyword: this.from.nokeyword,
+            frequency: this.from.frequency,
+            sign:sign,
+            uid:userInfo.id,
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       }
   },
   data(){
@@ -67,7 +89,10 @@ export default {
 
         }],
         form: {
-          name: '',
+          keyword: '',
+          may_keywor:'',
+          nokeyword:'',
+          frequency:''
     
         },
     }
