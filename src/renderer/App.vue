825c8fcc05dde3cf2c361e5513fc2e55b898509b
dezhizhang <1541609448@qq.com>
2018-09-19 20:10:12 +0800

    情服页面的制作

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -28,10 +28,11 @@
                 </el-submenu>
               </el-submenu>
               <el-submenu index="3">
-                <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+                <template slot="title"><i class="el-icon-setting"></i >情报设置</template>
                 <el-menu-item-group>
-                  <template slot="title">分组一</template>
-                  <el-menu-item index="3-1">选项1</el-menu-item>
+                  <el-menu-item index="3-1">
+                      <router-link :to="{ path:'/keyword'}">关建字设置 </router-link>
+                  </el-menu-item>
                   <el-menu-item index="3-2">选项2</el-menu-item>
                 </el-menu-item-group>
                 <el-menu-item-group title="分组2">
--- src/renderer/components/keywords/Keywords.vue
+++ src/renderer/components/keywords/Keywords.vue
@@ -1,18 +1,84 @@
 <template>
   <div class='Keywords'>
-     我是关建词报表
+     <div class='keywords-btn'>
+        <el-button size='small' type='primary' @click='addKeyWords'>增加关建字</el-button>
+     </div>
+     <div class='keywords-form'>
+        <el-dialog title="收货地址" :visible.sync="keywordsVisible">
+            <el-form :model="form">
+              <el-form-item label="标题" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="必须包含关建字" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="可包含关建字" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="不包含关建字" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="监控效果" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-form-item>
+             </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button size='small' @click="dialogFormVisible = false">取 消</el-button>
+                <el-button size='small' type="primary" @click="keywordsVisible = false">确 定</el-button>
+            </div>
+        </el-dialog>
+     </div>
+     <div class='keywords-table'>
+        <el-table :data="tableData" border style="width: 100%">
+          <el-table-column prop="date" label="标题" width="190">
+          </el-table-column>
+          <el-table-column prop="name" label="必须包含关建字" width="190">
+          </el-table-column>
+          <el-table-column prop="address" label="可包含关建字" width="190">
+          </el-table-column>
+           <el-table-column prop="address" label="不包含关建字" width="190"> 
+          </el-table-column>
+           <el-table-column prop="address" label="监控效果" width="190">
+          </el-table-column>
+        </el-table> 
+     </div>
+    <div class='keywords-pagination'>
+      <el-pagination background layout="prev, pager, next" :total="1000">
+      </el-pagination>
+    </div>
   </div>
-</template>
+</template> 
 
 <script>
 export default {
   name:'Keywords',
+  methods:{
+      addKeyWords(){
+        this.keywordsVisible = true;
+        
+
+      }
+  },
+  data(){
+    return{
+        keywordsVisible: false,
+        formLabelWidth: '120px',
+        tableData: [{
+
+        }],
+        form: {
+          name: '',
+    
+        },
+    }
+  }
   
 }
 </script>
 
-<style>
+<style scoped>
+.keywords-pagination{
+  float:right;
 
-
-
+}
 </style>
